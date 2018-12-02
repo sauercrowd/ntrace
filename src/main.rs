bd8c9d2f8d4b5fd20ffa052d44cf175d334d59e0
sauercrowd <sauercrowd@posteo.no>
2018-12-02 22:17:25 +0000

    fixed flow

--- src/main.rs
+++ src/main.rs
@@ -8,28 +8,50 @@ use nix::sys::signal::Signal;
 use nix::sys::ptrace;
 use std::mem;
 
+//fn attach(pid: i64) -> Result<
+
+
+
 fn main() {
-    let pid = nix::unistd::Pid::from_raw(28718);
+    let pid = nix::unistd::Pid::from_raw(1579);
 
     let attach = ptrace::attach(pid);
     println!("Attach {:?}", attach);
-    let m: *mut libc::c_void;
 
-    unsafe{
-        let m = libc::malloc(mem::size_of::<i64>()) as *mut libc::c_void;
-        let addr: ptrace::AddressType = m as *mut ffi::c_void;
+    match waitpid(pid, Some(<WaitPidFlag>::WSTOPPED)){
+        Ok(res) =>{
+            println!("First stop {:?}", res);
+            let scall = ptrace::syscall(pid);
+            println!("First scall {:?}", scall);
+        },
+        Err(res) => println!("{:?}", res)
     }
 
+    //buffer
+    let m: *mut libc::c_void;
 
-    for x in 0..10{
+    let m = unsafe { libc::malloc(1024*mem::size_of::<i64>()) as *mut libc::c_void };
+    let addr: ptrace::AddressType = m as *mut ffi::c_void;
+
+
+    loop{
         match waitpid(pid, Some(<WaitPidFlag>::WSTOPPED)) {
-            Ok(WaitStatus::Stopped(pid, Signal::SIGSTOP)) => {
-            let syscall = nix::sys::ptrace::syscall(pid);
-            println!("Syscall {:?}", syscall);
+            Ok(res) => {
+                println!("Res {:?}", res);
+                let a = unsafe {ptrace::ptrace(ptrace::Request::PTRACE_GETREGS, pid, addr, std::ptr::null_mut())};
+                println!("Address {:?}", addr);
 
-            let info = ptrace::getsiginfo(pid);
-            let r = ptrace::cont(pid, None);
-            println!("Cont {:?}", r)
+                let read_content = ptrace::read(pid, addr);
+                match read_content{
+                    Ok(c) => println!("MEMORY {:?}", c),
+                    _ => println!("NO MEMORY"),
+                }
+                let syscall = nix::sys::ptrace::syscall(pid);
+                println!("Syscall {:?}", syscall);
+
+
+//                let r = ptrace::cont(pid, None);
+//                println!("Cont {:?}", r)
             }
             _ => {},
         }
